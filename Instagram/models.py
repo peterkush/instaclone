39049ef055cbbd1e6ee4c4fed437838b649d610a
peterkush <pkungu004@gmail.com>
2021-01-19 14:35:43 +0300

    import IntegrityError to debug login and sign in form

--- Instagram/models.py
+++ Instagram/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.db import IntegrityError, models, router, transaction
 
 
 # Create your models here.
